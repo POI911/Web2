a0b9210a4c08b5251cbf775d89e73c90e7d551e6
unknown <abodoom539@gmail.com>
2021-06-05 17:31:16 +0300

    shiity code

--- marks.php
+++ marks.php
@@ -24,18 +24,42 @@ if(isset($_POST["submit"])){
 
     $GLOBALS['connection_link'];
     if(login() === true){
-        $_SESSION['auth'] = 1;
-        include "show.php";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "students_marks";
 
+        // Create connection again!
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-    }
+    $sql =  "SELECT S_NAME, C_NAME, C_MARK  FROM marks WHERE S_ID = '{$GLOBALS['studentLogin']}'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+
+echo "<style> table, th, td { border: 1px solid black; padding: 5px;} </style>";
+            echo "<table><tr><th>NAME</th><th>COURSE</th><th>MARK</th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<tr><td>".$row["S_NAME"]."</td><td>".$row["C_NAME"]."</td><td>".$row["C_MARK"]."</td></tr>"."</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+
+        $conn->close();
+}
+
     else{
         echo "Student ID or Password is wrong!";
         echo "<br>";
         echo "<a  href='index.html'> Try Again!</a>";
     }
-
-
 
     include "DBconnectionClose.php";
 }
@@ -43,6 +67,5 @@ else{
     echo "<h1>Access Denied!</h1>";
     echo "<a  href='index.html'> Go back</a>";
 }
-
 
 ?>
--- show.php
+++ show.php
@@ -6,10 +6,33 @@ echo "<h1>No permission!</h1>";
 exit();
 
 }
+
 else{
+        include "DBconnection.php";
 
-echo "Hello";
+        $GLOBALS['connection_link2'];
+        $query = "SELECT * FROM marks WHERE S_ID = '{$GLOBALS['studentLogin']}'  ";
+        if($result = mysqli_query($GLOBALS['connection_link2'], $query)){
+            if(mysqli_num_rows($result) == '1'){
+                   return true;
+            }
+            else return false;
+        }
+        else echo "connection bad";
+
+
+    if ($result->num_rows > 0) {
+        echo "<html><head></head><body>";
+        echo "<table><tr><th>ID</th><th>Name</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "<tr><td>".$row["S_NAME"]."</td><td>".$row["C_NAME"]." ".$row["C_MARK"]."</td></tr>";
+        }
+        echo "</table></body></html>";
+
+      }
+       else {
+        echo "0 results";
 }
-
-
 ?>
+
